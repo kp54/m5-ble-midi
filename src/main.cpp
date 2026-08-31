6c364ad1074624b4b15a4032ba4a32fd1380e9fe
kp54 <kangpang65@gmail.com>
2026-09-01 03:25:19 +0900

    change tone by touch

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,73 @@
 #include <M5Unified.h>
 
-extern const char* INSTRUMENT_NAMES[];
+extern const char *INSTRUMENT_NAMES[];
 const char TEXT_FACTOR = 8;
 
-char g_tone = 0;
-M5Canvas canvas;
+unsigned char g_tone = 0;
+unsigned char g_volume = 0;
+bool g_is_touched = false;
+M5Canvas g_canvas;
 
 void setup()
 {
     auto cfg = M5.config();
     M5.begin(cfg);
 
-    canvas.createSprite(M5.Display.width(), M5.Display.height());
+    g_canvas.createSprite(M5.Display.width(), M5.Display.height());
 }
 
 void paint(char tone)
 {
-    canvas.clearDisplay(TFT_BLACK);
-    canvas.setCursor(0, 0);
+    g_canvas.clearDisplay(TFT_BLACK);
+    g_canvas.setCursor(0, 0);
 
-    canvas.setTextSize(4);
-    canvas.printf("tone: %i\n", tone + 1);
+    g_canvas.setTextSize(4);
+    g_canvas.printf("tone: %i\n", tone + 1);
 
-    canvas.setCursor(0, TEXT_FACTOR * (4 + 1));
-    canvas.setTextSize(3);
-    canvas.printf("%s\n", INSTRUMENT_NAMES[tone]);
-    canvas.drawFastHLine(0, TEXT_FACTOR * 8, canvas.width(), TFT_WHITE);
+    g_canvas.setCursor(0, TEXT_FACTOR * (4 + 1));
+    g_canvas.setTextSize(3);
+    g_canvas.printf("%s\n", INSTRUMENT_NAMES[tone]);
+    g_canvas.drawFastHLine(0, TEXT_FACTOR * 8, g_canvas.width(), TFT_WHITE);
 
     M5.Display.startWrite();
-    canvas.pushSprite(&M5.Display, 0, 0);
+    g_canvas.pushSprite(&M5.Display, 0, 0);
     M5.Display.endWrite();
 }
 
 void loop()
 {
+    M5.update();
+    int display_width = M5.Display.width();
+    int display_height = M5.Display.height();
+
+    if (!g_is_touched)
+    {
+        for (char i = 0; i < M5.Touch.getCount(); i++)
+        {
+            auto touch = M5.Touch.getDetail(i);
+
+            if (0 <= touch.y && touch.y < TEXT_FACTOR * 8)
+            {
+                if (0 <= touch.x && touch.x < 64)
+                    g_tone--;
+                if (display_width - 64 <= touch.x && touch.x < display_width)
+                    g_tone++;
+            }
+        }
+
+        if (192 <= g_tone && g_tone < 256)
+            g_tone = 127;
+        if (128 <= g_tone && g_tone < 192)
+            g_tone = 0;
+
+        g_is_touched = true;
+    }
+
+    if (g_is_touched && M5.Touch.getCount() == 0)
+    {
+        g_is_touched = false;
+    }
+
     paint(g_tone);
-
-    g_tone++;
-    if (127 < g_tone)
-        g_tone = 0;
-
-    delay(100);
+    delay(1);
 }
