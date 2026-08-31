4fbdfd06caa13eb09eda9201485754eb1fdb646f
kp54 <kangpang65@gmail.com>
2026-09-01 03:06:09 +0900

    show instrument names

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,44 @@
-#include <M5Unified.h> // Make the M5Unified library available to your program.
+#include <M5Unified.h>
 
-// global variables (define variables to be used throughout the program)
-uint32_t count;
+extern const char* INSTRUMENT_NAMES[];
+const char TEXT_FACTOR = 8;
 
+char g_tone = 0;
+M5Canvas canvas;
 
-// setup function is executed only once at startup.
-// This function mainly describes the initialization process.
-void setup() {
+void setup()
+{
+    auto cfg = M5.config();
+    M5.begin(cfg);
 
-  auto cfg = M5.config(); // Assign a structure for initializing M5Stack
-  // If config is to be set, set it here
-  // Example.
-  // cfg.external_spk = true;
-
-  M5.begin(cfg);                        // initialize M5 device
-
-  M5.Display.setTextSize(3);            // change text size
-  M5.Display.print("Hello World!!!") ;  // display Hello World! and one line is displayed on the screen
-  Serial.println("Hello World!!!") ;    // display Hello World! and one line on the serial monitor
-  count = 0;                            // initialize count
-
+    canvas.createSprite(M5.Display.width(), M5.Display.height());
 }
 
-// loop function is executed repeatedly for as long as it is running.
-// loop function acquires values from sensors, rewrites the screen, etc.
-void loop() {
+void paint(char tone)
+{
+    canvas.clearDisplay(TFT_BLACK);
+    canvas.setCursor(0, 0);
 
-  M5.Display.setCursor(0, 20);             // set character drawing coordinates (cursor position)
-  M5.Display.printf("COUNT: %d\n", count); // display count on screen
-  Serial.printf("COUNT: %d\n", count);  // display count serially
-  count++;                              // increase count by 1
-  delay(1000);                          // wait 1 second(1,000msec)
+    canvas.setTextSize(4);
+    canvas.printf("tone: %i\n", tone + 1);
 
+    canvas.setCursor(0, TEXT_FACTOR * (4 + 1));
+    canvas.setTextSize(3);
+    canvas.printf("%s\n", INSTRUMENT_NAMES[tone]);
+    canvas.drawFastHLine(0, TEXT_FACTOR * 8, canvas.width(), TFT_WHITE);
+
+    M5.Display.startWrite();
+    canvas.pushSprite(&M5.Display, 0, 0);
+    M5.Display.endWrite();
+}
+
+void loop()
+{
+    paint(g_tone);
+
+    g_tone++;
+    if (127 < g_tone)
+        g_tone = 0;
+
+    delay(100);
 }
