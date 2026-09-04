34a01f55701112a9c9abd32f9c771ebc1873325b
kp54 <kangpang65@gmail.com>
2026-09-05 04:46:13 +0900

    connect ble device

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <M5Unified.h>
 #include <M5_SAM2695.h>
+#include <BLEMidi.h>
 
 extern const char *INSTRUMENT_NAMES[];
 const char TEXT_FACTOR = 8;
@@ -8,6 +9,9 @@ const char LOOP_DELAY = 1;
 unsigned char g_tone = 0;
 unsigned char g_volume = 63;
 bool g_is_touched = false;
+bool g_do_ble_scan = false;
+int g_ble_index = 0;
+int g_ble_devices = 0;
 M5Canvas g_canvas;
 M5_SAM2695 g_midi;
 
@@ -31,6 +35,21 @@ void paint()
     g_canvas.printf("volume: %i\n", g_volume);
     g_canvas.drawFastHLine(0, TEXT_FACTOR * 15, width, TFT_WHITE);
 
+    g_canvas.setCursor(0, TEXT_FACTOR * 16);
+    g_canvas.setTextSize(4);
+    if (g_ble_devices == 0)
+    {
+        g_canvas.printf("device: n/a\n");
+    }
+    else
+    {
+        g_canvas.printf("device: %i/%i\n", g_ble_index + 1, g_ble_devices);
+        g_canvas.setCursor(0, TEXT_FACTOR * 21);
+        g_canvas.setTextSize(2);
+        g_canvas.println(BLEMidiClient.deviceName(g_ble_index));
+    }
+    g_canvas.drawFastHLine(0, TEXT_FACTOR * 23, width, TFT_WHITE);
+
     M5.Display.startWrite();
     g_canvas.pushSprite(&M5.Display, 0, 0);
     M5.Display.endWrite();
@@ -40,8 +59,10 @@ void apply_values()
 {
     g_midi.setInstrument(0, 0, g_tone);
     g_midi.setMasterVolume(g_volume);
-
-    g_midi.setNoteOn(0, NOTE_A4, 127);
+    if (0 < g_ble_devices)
+    {
+        BLEMidiClient.connect(g_ble_index);
+    }
 }
 
 bool handle_touch()
@@ -79,6 +100,16 @@ bool handle_touch()
             if (display_width - 64 <= touch.x && touch.x < display_width)
                 g_volume++;
         }
+
+        if (TEXT_FACTOR * 16 <= touch.y && touch.y < TEXT_FACTOR * 24)
+        {
+            if (0 <= touch.x && touch.x < 64)
+                g_do_ble_scan = true;
+            if (display_width - 64 <= touch.x && touch.x < display_width)
+                g_ble_index++;
+            if (g_ble_devices <= g_ble_index)
+                g_ble_index = 0;
+        }
     }
 
     if (192 <= g_tone && g_tone < 256)
@@ -94,13 +125,38 @@ bool handle_touch()
     return true;
 }
 
+void handle_ble()
+{
+    if (!g_do_ble_scan)
+        return;
+    g_do_ble_scan = false;
+
+    g_ble_index = 0;
+    g_ble_devices = BLEMidiClient.scan();
+    if (0 < g_ble_devices)
+        BLEMidiClient.connect(0);
+}
+
 void setup()
 {
     auto cfg = M5.config();
     M5.begin(cfg);
+    Serial.begin(115200);
 
     g_canvas.createSprite(M5.Display.width(), M5.Display.height());
     g_midi.begin(&Serial2, MIDI_BAUD, 16, 17);
+
+    BLEMidiClient.setNoteOnCallback([](u8_t channel, u8_t note, u8_t velocity, u16_t timestamp)
+                                    { Serial.printf("NoteOn: channel=%i note=%i velocity=%i timestamp=%i\r\n", channel, note, velocity, timestamp); });
+    BLEMidiClient.setNoteOffCallback([](u8_t channel, u8_t note, u8_t velocity, u16_t timestamp)
+                                     { Serial.printf("NoteOff: channel=%i note=%i velocity=%i timestamp=%i\r\n", channel, note, velocity, timestamp); });
+
+    BLEMidiClient.setOnConnectCallback([]()
+                                       { Serial.printf("connected\r\n"); });
+    BLEMidiClient.setOnDisconnectCallback([]()
+                                          { Serial.printf("disconnected\r\n"); });
+
+    BLEMidiClient.begin("kp54");
 
     apply_values();
     paint();
@@ -113,6 +169,7 @@ void loop()
     if (handle_touch())
     {
         apply_values();
+        handle_ble();
         paint();
     }
 
